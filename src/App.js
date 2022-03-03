61377a6e46333cd053b1ddb0e2a7002b29f36dbb
seeun214 <seeunjin22@naver.com>
2022-03-03 19:51:37 +0900

    Feat: 검색어 구분 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useFetch from './hooks/useFetch';
+import serchItems from './utils/serchItems';
+import keywordSet from './lang/keyword.json';
 
 function App() {
-  return <div></div>;
+  const { loading: ploading, data: productsData } = useFetch({
+    requestUrl: 'https://static.pxl.ai/problem/data/products.json',
+  });
+  const { loading: rloading, data: regionsData } = useFetch({
+    requestUrl: 'https://static.pxl.ai/problem/data/regions.json',
+  });
+
+  const [list, setList] = useState([]);
+  const [keyword, setKeyword] = useState('');
+
+  const { distinguishKeyword } = serchItems({
+    ploading,
+    productsData,
+    rloading,
+    regionsData,
+    keyword,
+    setList,
+    keywordSet,
+  });
+  console.log(list);
+  return (
+    <div>
+      <input
+        type="text"
+        value={keyword}
+        onChange={(e) => {
+          setKeyword(e.target.value);
+        }}
+      ></input>
+      <button onClick={distinguishKeyword}>검색</button>
+    </div>
+  );
 }
 
 export default App;
